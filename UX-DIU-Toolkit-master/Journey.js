f577315e3738232a1e83f42a9b8fcb45fcd45eaf
romanlarrosa <58295696+romanlarrosa@users.noreply.github.com>
2020-03-05 13:33:14 +0100

    Update Journey.js

--- UX-DIU-Toolkit-master/Journey.js
+++ UX-DIU-Toolkit-master/Journey.js
@@ -88,7 +88,7 @@ angular.module("angular", [])
                 /*************************************/
                 
 				Id: 1,
-				Name: "Monica Suarez",
+				Name: "Guille. Nieto",
                 Photo: "woman.png",
                 
 				 /*** PASO #1: INSPIRACION ***/ 
