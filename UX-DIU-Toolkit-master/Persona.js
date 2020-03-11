2dc66ee666cfd266750f121d6789aebbc008e9cf
romanlarrosa <58295696+romanlarrosa@users.noreply.github.com>
2020-03-11 15:25:18 +0100

    Update Persona.js

--- UX-DIU-Toolkit-master/Persona.js
+++ UX-DIU-Toolkit-master/Persona.js
@@ -32,19 +32,23 @@ angular.module("angular", [])
 				Photo: "ashley.jpg",
 				Quote: "Kind heart, fierce mind and brave spirit",
 				Age: 21,
-				Occupation: "Estudiante de Erasmus",
-				Family: "En una relación a distancia",
-				Location: "Granada (Centro)",
-				Character: "Buenos momentos en compañía",
+				Occupation: "Estudiante Internacional",
+				Family: "En una relación. Familia tradicional, una hermana menor.",
+				Location: "EEUU",
+				Character: "Sosegada y risueña. Su mayor debilidad es compararse con los demás y priorizarse demasiado a ella misma y no a los que la rodean",
 				PersonalityTraits: [
 					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 5 },
 					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 4 },
 					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 4 },
 					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 3 }
 				], 
-				Goals: ["Destacar entre sus compañeros de clase", "Viajar por toda España", "Crear un canal de make-up en Youtube"],
-				Frustrations: ["No sabe si volver a EEUU o quedarse en España", "No encontrar trabajo al acabar sus estudios"],
-				Bio: "Estudiante de Erasmus proveniente de EEUU, del estado de Utah y que viene a Granada a cursar su cuarto curso de Marketing en la UGR. Tiene muchos amigos tanto en EEUU como en Granada, adora las actividades al aire libre y documentarlo todo en sus RRSS. ",
+				Goals: ["Destacar entre sus compañeros de clase", "Conocer lugares nuevos", "Generar contenido para sus RRSS", "Crear un negocio online"],
+				Frustrations: ["No satisfacer las expectativas de sus padres", "No saber gestionar su dinero a la hora de hacer compras", "No ser capaz de llevar una relacion a distancia", "No encajar entre los españoles"],
+				Bio: "Ashley nació en el estado de Utah, en  EEUU. Se ha criado en un ambiente muy cálido y su familia procesa una gran fe en la iglesia. Sus padres son dueños de una famosa inmobiliaria de su ciudad, por lo que nunca ha tenido problemas económicos. " +
+				"Ashley quiere continuar con el negocio familiar. Para ello está formandose en la universidad en el grado de Marketing. A Ashley le encantan las RRSS y las compras online, y cree que el marketing digital puede suponer un antes y un después en el negocio familiar. "+
+				"Es una apasionada de los animales de compañía, sin embargo, tristemente nunca ha podido tener uno debido a la terrible alergia que sufre su hermana Meredith. "+ 
+				"Le gusta ocupar su tiempo libre con actividades de ocio tales como ir de compras, visitar el cine, comer o cenar fuera, dado su bajo nivel culinario, y acudir a todo tipo de aconteciminetos sociales. "+
+				"Ashley mantiene desde hace 2 años una relación con Liam, con el que tiene pensado casarse una vez ambos terminen sus estudios. Este año Ashley será estudiante internacional en la UGR.",
 				Tech: [
 					{ Name: "TIC/Internet", Value: 1 },
 					{ Name: "Movil", Value: 4 },
@@ -52,7 +56,8 @@ angular.module("angular", [])
 					{ Name: "Software", Value: 1 }
 					
 				], 
-                Contextos: "La familia de Ashley está pagando su Erasmus y por ello no dispone de mucho dinero en este momento. Sin embargo, quiere cumplir a toda costa su meta de viajar por todos los rincones de España.",  
+				Contextos: "La familia de Ashley ha decidido poner a prueba su autonomía. Es por ello que va a disponer de un presupuesto muy limitado durante su estancia en España. Sin embargo, quiere cumplir a toda costa su meta de viajar por todos los rincones del país y compartirlo en sus RRSS. "+
+				"Ashley tiene miedo de que su relación se vea afectada por la distancia una vez se vaya de su ciudad. Su novio y ella confian mucho el uno en el otro, pero están muy unidos y nunca han estado tanto tiempo separados.",  
 				PreferredChannels: [
 					{ Name: "Publicidad Tradicional", Value: 4 },
 					{ Name: "Online & Social Media", Value: 5 },
@@ -71,11 +76,11 @@ angular.module("angular", [])
 				Id: 1,
 				Name: "Guillermo Nieto",
 				Photo: "guillermo.jpg",
-				Quote: "Tranquilo y hogareño",
-				Age: 27,
+				Quote: "Si todo falla: radioafición",
+				Age: 34,
 				Occupation: "Buscando un empleo",
 				Family: "Su padre y su madre, viven en su ciudad natal.",
-				Location: "Ahora mismo en Granada, pero quiere independizarse e irse a vivir a Madrid.",
+				Location: "Madrid",
 				Character: "Tímido, pero le encanta pasar tiempo con sus amigos más cercanos.",
 				PersonalityTraits: [
 					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 1 },
@@ -83,9 +88,11 @@ angular.module("angular", [])
 					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 2 },
 					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 3 }
 				], 
-				Goals: ["Conseguir independizarse y comenzar una vida por su cuenta", "Encontrar un piso con compañeros afines", "Conseguir el trabajo de sus sueños"],
-				Frustrations: ["No ser suficientemente bueno en las entrevistas de trabajo", "No conseguir conectar con sus futuros compañeros de piso", "Quedarse sin dinero antes de encontrar un trabajo"],
-				Bio: "Guillermo ha estudiado en granada la carrera de Telecomunicaciones, y ha finalizado tambien un máster. Tras trabajar un par de años como becario en algunas empresas de su ciudad, se dispone a buscar un empleo en el que pueda desarrollarse más personalmente. Siempre ha vivido con sus padres por lo que no ha tenido que convivir mucho tiempo con otra gente, y además es muy tímido y le encanta que su casa sea su remanso de paz y concentración. Guillermo necesita mucha confianza para poder desarrollar una relación con alguien, y le encantaría que sus compaleros de piso se convirtiesen tambien en sus amigos. Necesita un piso económico, por el momento, ya que hasta que encuentre un empleo, deberá subsistir con el dinero que ha ahorrado trabajando como becario. Guillermo necesita encontrar el piso lo antes posibe, ya que quiere empezar a buscar su futuro empleo cuanto antes.",
+				Goals: ["Escalar puestos en la empresa", "Ampliar su red de radioaficionados", "Intentar ser más extrovertido"],
+				Frustrations: ["No ser capaz de exteriorizar sus sentimientos", "Miedo a dejar solos a sus padres", "No encontrar amigos con los que compartir sus aficiones", ],
+				Bio: "Guillermo nació en Granada en una familia humilde. Es hijo único y sus padres siempre se han dedicado a la agricultura. Su deseo de mejorar la situación económica familiar y su pasión por la radiofonía le hicieron cursar y finalizar un grado de Telecomunicaciones en la universidad de su ciudad. "+
+				"Tras trabajar un par de años como becario en algunas empresas de Granada, Guillermo encontró un empleo en Madrid, por lo que se mudó a la capital. Guillermo tiene un carácter muy tranquilo, y le gusta llevar una vida solitaria. Es muy tímido por lo que siempre le ha costado hacer amistades. Sin embargo, la radioafición le ha abierto una puerta a conocer gente de manera que no sienta su intimidad ultrajada. "+
+				"Otra de sus aficiones destacables es la música, la cual le ha ayudado tambien a forjar amistades en su nueva ciudad. Guillermo tiene junto a dos amigos un grupo de jazz en el que toca el trombón. No está pasando un buen momento, ya que acaba de dejarlo con su pareja, con la cual llevaba 2 años de relación y acababa de instalarse en su casa. La convivencia, ha acabado con la relación.",
 				Tech: [
 					{ Name: "TIC/Internet", Value: 5 },
 					{ Name: "Mobile", Value: 2 },
@@ -93,7 +100,7 @@ angular.module("angular", [])
 					{ Name: "Software", Value: 4 }
 					
 				], 
-                Contextos:   "A Guillermo no le gusta pasar demasiadas horas innecesarias al ordenador, y mucho menos al móvil, que usa solo para lo necesario, pero está dispuesto a lo que sea para conseguir sus metas" ,
+                Contextos:   "Al estar alejado de sus padres, y ser estos muy mayores, han decidido ingresar en una residencia de ancianos. Guillermo no está muy de acuerdo, pero entiende que alli estarán mejor. Sin embargo, la pensión de sus padres no cubre totalmente los servicios de la residencia, por lo que Guillermo debe destinar parte de sus ingresos a la cuota mensual requerida en el centro." ,
 				PreferredChannels: [
 					{ Name: "Publicidad Tradicional (Ads)", Value: 3 },
 					{ Name: "Online & Social Media", Value: 1 },
